118ef05c80a89db3416a0760daf42c0725a0a034
saintsungminpark@gmail.com <saintsungminpark@gmail.com>
2020-02-15 19:45:04 +0900

    add key, prod files

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const { User } = require("./models/User")
 
+const config = require('./config/key');
+
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}))
 //application/json
 app.use(bodyParser.json())
 
-mongoose.connect(`mongodb+srv://sungminpark:1234@cluster0-gwmqe.mongodb.net/test?retryWrites=true&w=majority`, {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!!!'))
 
 app.post('/register', (req, res) => {
     //회원가입할때 필요한 정보들을 client에서 가져오면
